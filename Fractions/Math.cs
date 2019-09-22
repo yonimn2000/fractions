f8a3f1110f39f88710fd1463ba9268e0fbad6772
Yonatan Mankovich <yonimn2000@gmail.com>
2019-09-22 15:41:27 -0400

    Made partial classes.

---
+++ Fractions/Math.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace YonatanMankovich.Fractions
+{
+    public partial class Fraction
+    {
+        public Fraction Simplify()
+        {
+            if (!IsSimplified())
+            {
+                if (Numerator == 0 && Denominator != 1)
+                    Denominator = 1;
+                while (Numerator % 1 != 0 || Denominator % 1 != 0) //If either one is not a whole number
+                {
+                    Numerator *= 10;
+                    Denominator *= 10;
+                }
+                //Fix 1.000 to 1
+                Numerator = Math.Round(Numerator);
+                Denominator = Math.Round(Denominator);
+                long biggestCommonDenominator = GetGreatestCommonDenominator((long)Numerator, (long)Denominator);
+                Numerator /= biggestCommonDenominator;
+                Denominator /= biggestCommonDenominator;
+                if ((Numerator < 0 && Denominator < 0) || (Numerator >= 0 && Denominator < 0))
+                {
+                    Numerator *= -1;
+                    Denominator *= -1;
+                }
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Uses the Euclidean algorithm.
+        /// </summary>
+        public long GetGreatestCommonDenominator(long a, long b)
+        {
+            return a == 0 ? b : GetGreatestCommonDenominator(b % a, a);
+        }
+
+        public Fraction Reciprocate()
+        {
+            decimal temp = Numerator;
+            Numerator = Denominator;
+            Denominator = temp;
+            return this;
+        }
+
+        public Fraction GetReciprocal()
+        {
+            return new Fraction(Reciprocate());
+        }
+
+        public Fraction Exponenciate(decimal exponent)
+        {
+            if (exponent < 0)
+            {
+                Fraction reciprocal = GetReciprocal();
+                Numerator = reciprocal.Numerator;
+                Denominator = reciprocal.Denominator;
+                exponent *= -1;
+            }
+            Numerator = (decimal)Math.Pow((double)Numerator, (double)exponent);
+            Denominator = (decimal)Math.Pow((double)Denominator, (double)exponent);
+            return this;
+        }
+
+        public Fraction Absolute()
+        {
+            Numerator = Math.Abs(Numerator);
+            Denominator = Math.Abs(Denominator);
+            return this;
+        }
+    }
+}
---
+++ Fractions/Operators.cs
@@ -0,0 +1,90 @@
+﻿namespace YonatanMankovich.Fractions
+{
+    public partial class Fraction
+    {
+        static public Fraction operator +(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(fraction1.Numerator * fraction2.Denominator + fraction2.Numerator * fraction1.Denominator, fraction1.Denominator * fraction2.Denominator);
+        }
+
+        static public Fraction operator -(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(fraction1.Numerator * fraction2.Denominator - fraction2.Numerator * fraction1.Denominator, fraction1.Denominator * fraction2.Denominator);
+        }
+
+        static public Fraction operator *(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(fraction1.Numerator * fraction2.Numerator, fraction1.Denominator * fraction2.Denominator);
+        }
+
+        static public Fraction operator /(Fraction fraction1, Fraction fraction2)
+        {
+            return fraction1 * fraction2.GetReciprocal();
+        }
+
+        static public Fraction operator +(Fraction fraction, decimal number)
+        {
+            return fraction + new Fraction(number);
+        }
+
+        static public Fraction operator -(Fraction fraction, decimal number)
+        {
+            return fraction - new Fraction(number);
+        }
+
+        static public Fraction operator *(Fraction fraction, decimal number)
+        {
+            return fraction * new Fraction(number);
+        }
+
+        static public Fraction operator /(Fraction fraction, decimal number)
+        {
+            return fraction / new Fraction(number);
+        }
+
+        static public explicit operator double(Fraction fraction)
+        {
+            return (double)(fraction.Numerator / fraction.Denominator);
+        }
+
+        static public explicit operator decimal(Fraction fraction)
+        {
+            return fraction.Numerator / fraction.Denominator;
+        }
+
+        static public explicit operator long(Fraction fraction)
+        {
+            return (long)fraction.Simplify().GetWhole();
+        }
+
+        static public bool operator ==(Fraction fraction1, Fraction fraction2)
+        {
+            return fraction1.Numerator == fraction2.Numerator && fraction1.Denominator == fraction2.Denominator;
+        }
+
+        static public bool operator !=(Fraction fraction1, Fraction fraction2)
+        {
+            return !(fraction1 == fraction2);
+        }
+
+        static public bool operator <(Fraction fraction1, Fraction fraction2)
+        {
+            return (decimal)fraction1 < (decimal)fraction2;
+        }
+
+        static public bool operator >(Fraction fraction1, Fraction fraction2)
+        {
+            return (decimal)fraction1 > (decimal)fraction2;
+        }
+
+        static public bool operator <=(Fraction fraction1, Fraction fraction2)
+        {
+            return (decimal)fraction1 <= (decimal)fraction2;
+        }
+
+        static public bool operator >=(Fraction fraction1, Fraction fraction2)
+        {
+            return (decimal)fraction1 >= (decimal)fraction2;
+        }
+    }
+}
